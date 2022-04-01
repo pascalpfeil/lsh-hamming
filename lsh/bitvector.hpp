ba3efff86e745f8416fbf190c98aaad50202a7eb
Pascal Pfeil <pfeilpascal@outlook.de>
2022-04-01 09:29:52 -0400

    Make clang-tidy happy

--- lsh/bitvector.hpp
+++ lsh/bitvector.hpp
@@ -8,7 +8,7 @@ namespace lsh {
 
 using vector_t = boost::dynamic_bitset<>;
 
-size_t hamming_distance(const vector_t &v1, const vector_t &v2) {
+inline size_t hamming_distance(const vector_t &v1, const vector_t &v2) {
   return (v1 ^ v2).count();
 }
 
